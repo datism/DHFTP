8305c69281b3b4b0ba8f5b992747bc4751b39687
hiep_td194278 <85179885+LaurelWreath1804@users.noreply.github.com>
2021-12-28 22:44:21 +0700

    Add Log Out

--- Server/EnvVar.h
+++ Server/EnvVar.h
@@ -1,39 +1,211 @@
-#pragma once
+// Server.cpp : Defines the entry point for the console application.
+//
 
-#define SERVER_ADDR "127.0.0.1"
-#define CMD_PORT 5500
-#define BUFFSIZE 8192
-#define ENDING_DELIMITER "\r\n"
-#define HEADER_DELIMITER "\r"
-#define PARA_DELIMITER " "
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+#include <sql.h>
+#include <string>
+#include <codecvt>
+using namespace std;
 
-enum REPLY_CODE {
-	LOGIN_SUCCESS = 110,
-	LOGOUT_SUCCESS = 111,
-	REGISTER_SUCCESS = 112,
-	
-	NOT_LOGIN = 310,
-	ALREADY_LOGIN = 311,
-	USER_NOT_EXIST = 312,
-	USER_ALREADY_EXIST = 313,
-	WRONG_PASSWORD = 314,
-	EMPTY_FIELD = 315,
-	
-	RETRIEVE_SUCCESS = 220,
-	STORE_SUCCESS = 221,
-	FINISH_SEND = 120,
-	RENAME_SUCCESS = 121,
-	DELETE_SUCCESS = 122,
-	MAKEDIR_SUCCESS = 123,
-	REMOVEDIR_SUCCESS = 124,
-	CHANGEWDIR_SUCCESS = 125,
-	PRINTWDIR_SUCCESS = 126,
-	LIST_SUCCESS = 127,
+#define SQL_RESULT_LEN 240
+#define SQL_RETURN_CODE_LEN 1000
 
-	NO_ACCESS = 320,
-	FILE_NOT_EXIST = 321,
-	FILE_ALREADY_EXIST = 322,
-	NAME_WRONG_FORMAT = 323,
+int main() {
+	string username, password;
+	int mode;
 
-	WRONG_SYNTAX = 330
-};
+	string query;
+	wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	wstring wstr;
+
+	SQLHANDLE sqlConnHandle;
+	SQLHANDLE sqlStmtHandle;
+	SQLHANDLE sqlEnvHandle;
+	SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
+
+	sqlConnHandle = NULL;
+	sqlStmtHandle = NULL;
+
+	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle))
+		goto COMPLETED;
+	if (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
+		goto COMPLETED;
+	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle))
+		goto COMPLETED;
+	//output
+	cout << "Attempting connection to SQL Server...";
+	cout << "\n";
+
+	switch (SQLDriverConnect(sqlConnHandle,
+		NULL,
+		(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=FileSystem;UID=sa;PWD=minh1234;",
+		SQL_NTS,
+		retconstring,
+		1024,
+		NULL,
+		SQL_DRIVER_NOPROMPT))
+	{
+	case SQL_SUCCESS:
+		cout << "Successfully connected to SQL Server";
+		cout << "\n";
+		break;
+	case SQL_SUCCESS_WITH_INFO:
+		cout << "Successfully connected to SQL Server";
+		cout << "\n";
+		break;
+	case SQL_INVALID_HANDLE:
+		cout << "Could not connect to SQL Server";
+		cout << "\n";
+		goto COMPLETED;
+	case SQL_ERROR:
+		cout << "Could not connect to SQL Server";
+		cout << "\n";
+		goto COMPLETED;
+	default:
+		break;
+	}
+
+	if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
+		goto COMPLETED;
+
+	cout << "Choose mode: 1: Log In   2: Sign Up    3: Log out" << endl;
+	cin >> mode;
+
+	getline(cin, username);
+	cout << "Enter username: ";
+	getline(cin, username);
+	cout << "Enter password: ";
+	getline(cin, password);
+
+	if (username.length() == 0 || password.length() == 0) {
+		//315
+		cout << "Empty field" << endl;
+	}
+	else if (mode == 1) {
+		cout << "\n";
+		cout << "Logging in...";
+		cout << "\n";
+
+		query = "SELECT * FROM Account where username='" + username + "'";
+		wstr = converter.from_bytes(query);
+
+		if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)wstr.c_str(), SQL_NTS)) {
+			cout << "Error querying SQL Server";
+			cout << "\n";
+		}
+
+		SQLCHAR sqlUsername[50];
+		SQLCHAR sqlPassword[50];
+		SQLCHAR sqlStatus[50];
+
+		if (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
+			SQLGetData(sqlStmtHandle, 1, SQL_CHAR, sqlUsername, sizeof(sqlUsername), NULL);
+			SQLGetData(sqlStmtHandle, 2, SQL_CHAR, sqlPassword, sizeof(sqlPassword), NULL);
+			SQLGetData(sqlStmtHandle, 3, SQL_CHAR, sqlStatus, sizeof(sqlStatus), NULL);
+
+			string strSqlPassword = reinterpret_cast<char*>(sqlPassword);
+			string strSqlStatus = reinterpret_cast<char*>(sqlStatus);
+
+			SQLCloseCursor(sqlStmtHandle);
+
+			if (password == strSqlPassword) {
+				if (strSqlStatus == "0") {
+					query = "UPDATE Account SET status = 1 WHERE username='" + username + "';";
+					wstr = converter.from_bytes(query);
+					wcout << wstr << endl;
+					if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)wstr.c_str(), SQL_NTS)) {
+						cout << "Error querying SQL Server";
+						cout << "\n";
+					}
+					else {
+						//110
+						cout << "Log in successful!" << endl;
+					}
+				}
+				else {
+					//311
+					cout << "Already logged in" << endl;
+				}
+			}
+			else {
+				//314
+				cout << "Wrong password" << endl;
+			}
+		}
+		else {
+			//312
+			cout << "Username doesn't exist" << endl;
+		}
+	}
+	else if (mode == 2) {
+		cout << "\n";
+		cout << "Signing up...";
+		cout << "\n";
+
+		query = "INSERT INTO Account VALUES ('" + username + "','" + password + "',0)";
+		wstr = converter.from_bytes(query);
+
+		if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)wstr.c_str(), SQL_NTS)) {
+			//313
+			cout << "Username already exists" << endl;
+		}
+		else {
+			//112
+			cout << "Sign up successful" << endl;;
+		}
+	}
+	else if (mode == 3) {
+		cout << "\n";
+		cout << "Logging out...";
+		cout << "\n";
+
+		query = "SELECT * FROM Account where username='" + username + "'";
+		wstr = converter.from_bytes(query);
+
+		if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)wstr.c_str(), SQL_NTS)) {
+			cout << "Error querying SQL Server";
+			cout << "\n";
+		}
+
+		SQLCHAR sqlStatus[50];
+
+		if (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
+			SQLGetData(sqlStmtHandle, 3, SQL_CHAR, sqlStatus, sizeof(sqlStatus), NULL);
+
+			string strSqlStatus = reinterpret_cast<char*>(sqlStatus);
+
+			SQLCloseCursor(sqlStmtHandle);
+
+			if (strSqlStatus == "1") {
+				query = "UPDATE Account SET status = 0 WHERE username='" + username + "';";
+				wstr = converter.from_bytes(query);
+				wcout << wstr << endl;
+				if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)wstr.c_str(), SQL_NTS)) {
+					cout << "Error querying SQL Server";
+					cout << "\n";
+				}
+				else {
+					//111
+					cout << "Log out successful!" << endl;
+				}
+			}
+			else {
+				//310
+				cout << "Log out failed. Didn't log in" << endl;
+			}
+		}
+	}
+
+COMPLETED:
+	SQLFreeHandle(SQL_HANDLE_STMT, sqlStmtHandle);
+	SQLDisconnect(sqlConnHandle);
+	SQLFreeHandle(SQL_HANDLE_DBC, sqlConnHandle);
+	SQLFreeHandle(SQL_HANDLE_ENV, sqlEnvHandle);
+
+	cout << "\nPress any key to exit...";
+	getchar();
+}
